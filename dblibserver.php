d70d6273a015e088d52f374d01c9c1500e82f6ae
Andre Alves Garzia <andre@andregarzia.com>
2017-06-06 18:37:10 +0000

    first working version of remote library

--- dblibserver.php
+++ dblibserver.php
@@ -6,18 +6,35 @@
  */
 
  // Override the data below with your defaults
- $encryption_password = "";
- $user = "";
- $password = "";
- $db = "";
+ $encryption_key = "FFFFFFFFFFFFDDCCFFFFFFFFFFFFDDCC";
+ $user = "test_dblib";
+ $password = "supersecret";
+ $db = "agarzia";
  $server = "localhost";
+ $cipher = "AES-256-CTR"; // do not change cipher unless you know what you're doing
+
+ function debug($msg) {
+     $debug = true;
+     if ($debug) {
+         error_log("[DB LIB] $msg");
+     }
+ }
 
  // decrypt the post
- $post = $_POST_RAW;
- $post = openssl_decrypt($post, "aes-256-ctr", $encryption_password);
+ $post = file_get_contents('php://input');
 
+ $post = openssl_decrypt($post, $cipher, $encryption_key);
+ if (!$post) {
+     debug("error on decrypt");
+     debug(openssl_error_string());
+ }
 
+ debug("Post: $post");
  $req = json_decode($post, true);
+
+ if (json_last_error() !== JSON_ERROR_NONE) {
+     $error = json_last_error();
+ }
 
  if (isset($req["user"])) {
      $user = $req["user"];
@@ -31,6 +48,8 @@
      $db = $req["db"];
  }
 
+ $sql = $req["sql"];
+
 
  $type = $req["type"];
 
@@ -43,21 +62,76 @@
  } else {
     switch($type) {
         case "execute":
-            $query = $mysqli->query($sql);
-            $retVal["affected_rows"] = $query->affected_rows;
+            if (isset($req["placeholders"])) {
+                // replace :x with ? in sql
+                for($i = 1; $i <= count($req["placeholders"]); $i++) {
+                    $sql = str_replace(":$i", "?", $sql);
+                }
+                debug("Prepared statement sql: $sql");
+                $stmt = $mysqli->prepare($sql);
+                $bindstr = "";
+                $n = count($req["placeholders"]);
+                for($i = 1; $i <= $n; $i++) {
+                   $bindstr .= "s";
+                }
+                
+                $a_params[] = & $bindstr;
+
+                for($i = 1; $i <= $n; $i++) {
+                    /* with call_user_func_array, array params must be passed by reference */
+                    $a_params[] = & $req["placeholders"][$i];
+                }
+                call_user_func_array(array($stmt, 'bind_param'), $a_params);
+                $succ = $stmt->execute();
+                if ($succ) {
+                    $retVal["affected_rows"] = $query->affected_rows;
+                } else {
+                    $retVal["error"] = $stmt->error;
+                    $retVal["sql"] = $sql;
+                }
+
+                $stmt->close();
+
+            } else {
+                $query = $mysqli->query($sql);
+                if (!$query) {
+                    $retVal["error"] = $mysqli->error;
+                    $retVal["sql"] = $sql;
+                } else {
+                    $retVal["affected_rows"] = $query->affected_rows;
+                    $query->close();
+                }
+            }
+            $mysqli->close();
+            
             break;
         case "query":
-            $query = $mysqli->query($sql);
-            $data = $query->mysqli_fetch_array();
-            $retVal["data"] = $data;
-            $retVal["num_rows"] = $query->num_rows;
+            try {
+                $query = $mysqli->query($sql);
+
+                if (!$query) {
+                    $retVal["error"] = $mysqli->error;
+                    $retVal["sql"] = $sql;
+                } else {
+
+                    while($row = $query->fetch_array(MYSQLI_ASSOC)) {
+                        $rows[] = $row;
+                    }                
+                    
+                    $retVal["data"] = $rows;
+                    $retVal["num_rows"] = $query->num_rows;
+                    $query->close();
+                }
+                $mysqli->close();
+            } catch(Exception $e) {
+                $retVal["error"] = "Exception in query";
+            }
             break;            
     }
  }
 
- header('Content-Type: text/plain');
- $retVal = openssl_decrypt(json_encode($retVal), "aes-256-ctr", $encryption_password);
+ header('Content-Type: text/plain+dblib');
+ $retVal = openssl_encrypt(json_encode($retVal), $cipher, $encryption_key);
 
  echo $retVal;
 
- // encrypt the result
