77db8d12542b0e25b561db60907e37c2b1daa53a
Andre Alves Garzia <andre@andregarzia.com>
2017-07-14 04:30:55 +0000

    fix(remote): placeholders not working

--- dblibserver.php
+++ dblibserver.php
@@ -3,15 +3,23 @@
 /*
  Remote DB Lib Server
 
- */
+*/
 
  // Override the data below with your defaults
  $encryption_key = "FFFFFFFFFFFFDDCCFFFFFFFFFFFFDDCC";
  $user = "test_dblib";
  $password = "supersecret";
- $db = "agarzia";
+ $dbname = "agarzia";
  $server = "localhost";
+ $database_type = "mysql";
  $cipher = "AES-256-CTR"; // do not change cipher unless you know what you're doing
+
+ // Disable errors display
+ ini_set('display_errors', 1); // set to false == disable display.
+ 
+ require_once("idiorm.php");
+
+/* Auxiliary function */
 
  function debug($msg) {
      $debug = true;
@@ -22,8 +30,8 @@
 
  // decrypt the post
  $post = file_get_contents('php://input');
-
  $post = openssl_decrypt($post, $cipher, $encryption_key);
+
  if (!$post) {
      debug("error on decrypt");
      debug(openssl_error_string());
@@ -45,90 +53,42 @@
  }
 
  if (isset($req["db"])) {
-     $db = $req["db"];
+     $dbname = $req["db"];
  }
+
+ if (isset($req["database_type"])) {
+     $database_type = $req["database_type"];
+ }
+
+ ORM::configure("${database_type}:host=${server};dbname=${dbname}");
+ ORM::configure('username', $user);
+ ORM::configure('password', $password);
 
  $sql = $req["sql"];
-
-
  $type = $req["type"];
-
  $retVal = [];
 
- $mysqli = new mysqli($server, $user, $password, $db);
-
- if (mysqli_connect_errno()) {
-    $retVal["error"] = mysqli_connect_error();
- } else {
-    switch($type) {
-        case "execute":
-            if (isset($req["placeholders"])) {
-                // replace :x with ? in sql
-                for($i = 1; $i <= count($req["placeholders"]); $i++) {
-                    $sql = str_replace(":$i", "?", $sql);
-                }
-                debug("Prepared statement sql: $sql");
-                $stmt = $mysqli->prepare($sql);
-                $bindstr = "";
-                $n = count($req["placeholders"]);
-                for($i = 1; $i <= $n; $i++) {
-                   $bindstr .= "s";
-                }
-                
-                $a_params[] = & $bindstr;
-
-                for($i = 1; $i <= $n; $i++) {
-                    /* with call_user_func_array, array params must be passed by reference */
-                    $a_params[] = & $req["placeholders"][$i];
-                }
-                call_user_func_array(array($stmt, 'bind_param'), $a_params);
-                $succ = $stmt->execute();
-                if ($succ) {
-                    $retVal["affected_rows"] = $query->affected_rows;
-                } else {
-                    $retVal["error"] = $stmt->error;
-                    $retVal["sql"] = $sql;
-                }
-
-                $stmt->close();
-
-            } else {
-                $query = $mysqli->query($sql);
-                if (!$query) {
-                    $retVal["error"] = $mysqli->error;
-                    $retVal["sql"] = $sql;
-                } else {
-                    $retVal["affected_rows"] = $query->affected_rows;
-                    $query->close();
-                }
-            }
-            $mysqli->close();
-            
-            break;
-        case "query":
-            try {
-                $query = $mysqli->query($sql);
-
-                if (!$query) {
-                    $retVal["error"] = $mysqli->error;
-                    $retVal["sql"] = $sql;
-                } else {
-
-                    while($row = $query->fetch_array(MYSQLI_ASSOC)) {
-                        $rows[] = $row;
-                    }                
-                    
-                    $retVal["data"] = $rows;
-                    $retVal["num_rows"] = $query->num_rows;
-                    $query->close();
-                }
-                $mysqli->close();
-            } catch(Exception $e) {
-                $retVal["error"] = "Exception in query";
-            }
-            break;            
-    }
+ switch($type) {
+     case "query":
+        if (isset($req["placeholders"])) {
+            $retVal = ORM::for_table($req["table"])->raw_query($sql, $req["placeholders"])->find_array();
+        } else {
+            $retVal = ORM::for_table($req["table"])->raw_query($sql)->find_array();
+        }
+     break;
+     case "execute":
+      if (isset($req["placeholders"])) {
+            $retVal = ORM::for_table($req["table"])->raw_query($sql, $req["placeholders"]);
+        } else {
+            $retVal = ORM::for_table($req["table"])->raw_query($sql);
+        }
+     break;
+     default:
+     break;
  }
+
+ //debug(print_r($retVal, true));
+
 
  header('Content-Type: text/plain+dblib');
  $retVal = openssl_encrypt(json_encode($retVal), $cipher, $encryption_key);
